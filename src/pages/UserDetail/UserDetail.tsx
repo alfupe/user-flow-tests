c26552cdc1529ec729079985f89d28daf109c1d8
Alex Fuentes <alfupe@gmail.com>
2025-07-08 00:23:04 +0200

    error handling

--- src/pages/UserDetail/UserDetail.tsx
+++ src/pages/UserDetail/UserDetail.tsx
@@ -2,25 +2,33 @@ import type { User } from 'features/user'
 import { useEffect, useState } from 'react'
 import { selectTheme, toggleTheme } from 'store/slices/theme'
 import { useAppDispatch, useAppSelector } from 'store/hooks'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const UserDetail = () => {
-  const slug = 'john-maverick'
+  const { slug } = useParams()
   const [user, setUser] = useState<User | null>(null)
+  const [error, setError] = useState(false)
   const theme = useAppSelector(selectTheme)
   const navigate = useNavigate()
   const dispatch = useAppDispatch()
 
   useEffect(() => {
+    if (!slug) return
     getUserBySlug(slug)
   }, [])
 
   const getUserBySlug = async (slug: string) => {
-    const userRequest = await fetch(`https://example.com/users/${slug}`)
-    const user = await userRequest.json()
-    setUser(user)
+    try {
+      setError(false)
+      const userRequest = await fetch(`https://example.com/users/${slug}`)
+      const user = await userRequest.json()
+      setUser(user)
+    } catch {
+      setError(true)
+    }
   }
 
+  if (error) return <h3>Usuario no encontrado</h3>
   if (!user) return null
 
   return (
--- src/pages/UserDetail/__tests__/UserDetail.test.tsx
+++ src/pages/UserDetail/__tests__/UserDetail.test.tsx
@@ -5,7 +5,6 @@ import { userEvent } from '@testing-library/user-event'
 
 it('renders the user`s detail page', async () => {
   window.history.pushState({}, '', '/john-maverick')
-
   render(<App />)
 
   expect(
@@ -23,6 +22,18 @@ it('renders the user`s detail page', async () => {
   expect(screen.getByRole('complementary')).toContainHTML('Tema light')
 })
 
+it('cannot render the user detail page since the user due to 404', async () => {
+  window.history.pushState({}, '', '/unknown-user-slug')
+  render(<App />)
+
+  expect(
+    await screen.findByRole('heading', {
+      name: 'Usuario no encontrado',
+      level: 3,
+    }),
+  )
+})
+
 it('toggles theme and navigates back to home', async () => {
   window.history.pushState({}, '', '/john-maverick')
   const user = userEvent.setup()
@@ -33,6 +44,8 @@ it('toggles theme and navigates back to home', async () => {
   await user.click(aside)
   await user.click(backButton)
 
-  expect(await screen.findByRole('heading', { name: 'User list', level: 1 }))
+  expect(
+    await screen.findByRole('heading', { name: 'User list', level: 1 }),
+  ).toBeVisible()
   expect(screen.getByRole('complementary')).toContainHTML('Tema dark')
 })
